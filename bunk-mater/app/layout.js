5fbc912b3e20318c33c8cac7e65417ce3321cdb7
amansxcalibur <notifyamanvs@gmail.com>
2025-01-11 11:04:18 +0530

    adds provider for user ID while logging

--- bunk-mater/app/layout.js
+++ bunk-mater/app/layout.js
@@ -1,6 +1,7 @@
-import { Urbanist } from 'next/font/google';
-import './globals.css';
-import GoogleAnalytics from '@/components/google_analytics/google_analytics';
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import GoogleAnalytics from "@/components/google_analytics/google_analytics";
+import { UserProvider } from "@/app/_contexts/user_name";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -10,15 +11,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-   return (
-      <html lang="en">
-         <head>
-            <link rel="icon" href="/icon.png" sizes="any" />
-         </head>
-         <body className={urbanist.className}>
-            <GoogleAnalytics/>
-            {children}
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" sizes="any" />
+      </head>
+      <body className={urbanist.className}>
+        <GoogleAnalytics />
+        <UserProvider>{children}</UserProvider>
+      </body>
+    </html>
+  );
 }
