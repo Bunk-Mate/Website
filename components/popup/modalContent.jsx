b765bba0b90a899eef6566478f2aaaf353d1cf29
amansxcalibur <notifyamanvs@gmail.com>
2025-03-16 19:43:12 +0530

    style: fix alignment issues in modal component

--- components/popup/modalContent.jsx
+++ components/popup/modalContent.jsx
@@ -4,9 +4,9 @@ export default function ModalContent({
    props,
 }) {
    return (
-      <div className="absolute top-0 flex h-full w-full items-center justify-center backdrop-blur-sm">
-         <div className="flex min-h-36 min-w-[300px] flex-col bg-black md:min-h-[200px] md:min-w-[500px]">
-            <div className="flex flex-1 items-center justify-center">
+      <div className="absolute top-0 flex size-full items-center justify-center backdrop-blur-sm">
+         <div className="flex min-h-36 min-w-[300px] flex-col rounded-[20px] bg-black shadow-[0px_0px_50px_rgb(50,50,50)] md:min-h-[200px] md:min-w-[500px]">
+            <div className="flex flex-1 items-center justify-center p-3 max-sm:max-w-[90vw]">
                {props.message}
             </div>
             <div className="flex">
